93f10008458a621647db408014c02e539de1af72
Marko van Treeck <markovantreeck@gmail.com>
2016-08-26 17:11:36 +0200

    Reformat tests

--- src/main.rs
+++ src/main.rs
@@ -31,6 +31,7 @@ fn parse_and_eval(input: &[u8]) -> Rc<Expr> {
 }
 
 #[cfg(test)]
+#[cfg_attr(rustfmt, rustfmt_skip)]
 mod tests {
     use expression::Expr;
     use super::parse_and_eval;
@@ -53,20 +54,28 @@ mod tests {
 
     #[test]
     fn lambda_with_list_arg() {
-        let prog = "(define second (lambda (xs) (head (tail xs)))) (second '(1 2 3))";
+        let prog = "(define second (lambda (xs) (head (tail xs)))) \
+                    (second '(1 2 3))";
         assert_eq!(*parse_and_eval(prog.as_bytes()), Expr::Number(2));
     }
 
     #[test]
     fn recursion() {
-        let prog = "(define count (lambda (down up) (if (= down 0) up (count (- down 1) (+ up \
-                    1))))) (count 3 0)";
+        let prog = "(define count \
+                      (lambda (down up) \
+                        (if (= down 0) \
+                            up \
+                            (count (- down 1) (+ up 1))))) \
+                    (count 3 0)";
         assert_eq!(*parse_and_eval(prog.as_bytes()), Expr::Number(3));
     }
 
     #[test]
     fn capture() {
-        let prog = "(define f (let ((x 1)) (lambda () x))) (f)";
+        let prog = "(define f \
+                      (let ((x 1)) \
+                        (lambda () x))) \
+                    (f)";
         assert_eq!(*parse_and_eval(prog.as_bytes()), Expr::Number(1));
     }
 
@@ -85,11 +94,11 @@ mod tests {
 
     #[test]
     fn currying() {
-        // single curry
+// single curry
         let prog = "(((lambda (x y) y) 1) 2)";
         assert_eq!(*parse_and_eval(prog.as_bytes()), Expr::Number(2));
 
-        // double curry
+// double curry
         let prog = "(((lambda (x y z) z) 1) 2) 3)";
         assert_eq!(*parse_and_eval(prog.as_bytes()), Expr::Number(3));
     }
@@ -105,36 +114,58 @@ mod tests {
 
     #[test]
     fn length() {
-        let prog = "(define length (lambda (xs) (if (nil? xs) 0 (+ 1 (length (tail xs)))))) \
+        let prog = "(define length \
+                      (lambda (xs) \
+                        (if (nil? xs) \
+                            0 \
+                          (+ 1 (length (tail xs)))))) \
                     (length '(1 2 3 4 5))";
         assert_eq!(*parse_and_eval(prog.as_bytes()), Expr::Number(5));
     }
 
     #[test]
     fn factorial() {
-        let prog = "(define fac (lambda (x) (if (= x 0) 1 (* x (fac (- x 1)))))) (fac 5)";
+        let prog = "(define fac \
+                      (lambda (x) \
+                        (if (= x 0) \
+                            1 \
+                          (* x (fac (- x 1)))))) \
+                    (fac 5)";
         assert_eq!(*parse_and_eval(prog.as_bytes()), Expr::Number(120));
     }
 
     #[test]
     fn gcd() {
-        let prog = "(define gcd (lambda (a b) (if (= b 0) a (gcd b (mod a b))))) (gcd 20 6)";
+        let prog = "(define gcd \
+                      (lambda (a b) \
+                        (if (= b 0) \
+                            a \
+                          (gcd b (mod a b))))) \
+                    (gcd 20 6)";
         assert_eq!(*parse_and_eval(prog.as_bytes()), Expr::Number(2));
     }
 
     #[test]
     fn fibonacci() {
-        let prog = "(define fib (lambda (n) (if (= n 0) 0 (if (= n 1) 1 (+ (fib (- n 1)) (fib (- \
-                    n 2))))))) (fib 10)";
+        let prog = "(define fib \
+                      (lambda (n) \
+                        (cond ((= n 0) 0) \
+                              ((= n 1) 1) \
+                              (true (+ (fib (- n 1)) (fib (- n 2))))))) \
+                    (fib 10)";
         assert_eq!(*parse_and_eval(prog.as_bytes()), Expr::Number(55));
     }
 
     #[test]
     fn own_list() {
-        let prog = "(define own-cons (lambda (a b) (lambda (left?) (if left? a b)))) (define \
-                    own-head (lambda (xs) (xs true))) (define own-tail (lambda (xs) (xs nil))) \
-                    (define list (own-cons 1 (own-cons 2 (own-cons 3 nil)))) (own-head (own-tail \
-                    list))";
+        let prog = "(define own-cons \
+                      (lambda (a b) \
+                        (lambda (left) \
+                          (if left a b)))) \
+                    (define own-head (lambda (xs) (xs true))) \
+                    (define own-tail (lambda (xs) (xs nil))) \
+                    (define list (own-cons 1 (own-cons 2 (own-cons 3 nil)))) \
+                    (own-head (own-tail list))";
         assert_eq!(*parse_and_eval(prog.as_bytes()), Expr::Number(2));
     }
 }
